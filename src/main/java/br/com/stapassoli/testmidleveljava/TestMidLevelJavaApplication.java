23372043858a0452bd13137cdf4bf616bc98e406
hugoodesa <hugoodesa@gmail.com>
2023-11-04 21:03:32 -0300

    Alterado projeto para que possa ser feito deploy com tomcat

--- src/main/java/br/com/stapassoli/testmidleveljava/TestMidLevelJavaApplication.java
+++ src/main/java/br/com/stapassoli/testmidleveljava/TestMidLevelJavaApplication.java
@@ -2,9 +2,10 @@ package br.com.stapassoli.testmidleveljava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestMidLevelJavaApplication {
+public class TestMidLevelJavaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestMidLevelJavaApplication.class, args);
--- src/main/java/br/com/stapassoli/testmidleveljava/controller/ChildController.java
+++ src/main/java/br/com/stapassoli/testmidleveljava/controller/ChildController.java
@@ -4,11 +4,13 @@ import br.com.stapassoli.testmidleveljava.DTO.ChildParentProjection;
 import br.com.stapassoli.testmidleveljava.service.ChildrenService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,6 +20,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class ChildController {
 
     private final ChildrenService childService;
+
+    @GetMapping("/hellow")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> sayHellow () {
+        return ResponseEntity.ok("Hellow");
+    }
 
     @PostMapping
     public ResponseEntity<?> createChild(@RequestParam("file") MultipartFile file) {
